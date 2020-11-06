07d50d09c6bc830dd46e07c43d1d6ac0b370af4d
nikpon <ponomarenkonikita33@gmail.com>
2020-11-06 16:17:50 +0200

    Initial Commit

---
+++ RxSwiftPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftPractice
+//
+//  Created by Nikita Ponomarenko on 06.11.2020.
+//  Copyright © 2020 Nikita Ponomarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
